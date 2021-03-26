b8c183ffc1048775b80a7ec360e2df4c750c2380
Deniz Bilgin <denizbilginGs27@hotmail.com>
2021-03-26 18:16:34 +0300

    26.03.2021 - second

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-navi',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService:AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  isAuthenticated(){
+    return this.authService.isAuthenticated();
+  }
 }
