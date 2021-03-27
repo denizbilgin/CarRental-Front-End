a84468aab07f0eb90af9a17eb99a4a819df1e7b2
Deniz Bilgin <denizbilginGs27@hotmail.com>
2021-03-27 21:10:29 +0300

    27.03.2021 - fourth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BrandAddComponent } from './components/brand-add/brand-add.component';
+import { BrandAddComponent } from './components/brand/brand-add/brand-add.component';
 import { BrandComponent } from './components/brand/brand.component';
-import { CarAddComponent } from './components/car-add/car-add.component';
-import { CarDetailComponent } from './components/car-detail/car-detail.component';
-import { CarUpdateComponent } from './components/car-update/car-update.component';
+import { CarAddComponent } from './components/car/car-add/car-add.component';
+import { CarDetailComponent } from './components/car/car-detail/car-detail.component';
+import { CarUpdateComponent } from './components/car/car-update/car-update.component';
 import { CarComponent } from './components/car/car.component';
-import { ColorAddComponent } from './components/color-add/color-add.component';
+import { ColorAddComponent } from './components/color/color-add/color-add.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { LoginComponent } from './components/login/login.component';
 import { PaymentComponent } from './components/payment/payment.component';
